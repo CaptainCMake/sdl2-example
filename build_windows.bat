ea0df5e4e2cd3daac643485cf9bb95725e6df8f3
Steve Robinson <ssrobins@gmail.com>
2019-06-09 17:12:12 -0700

    Add systeminfo call

--- build_windows.bat
+++ build_windows.bat
@@ -9,6 +9,8 @@ cd %build_dir%
 
 conan install .. -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
 
+systeminfo
+
 cmake -G "Visual Studio 16 2019" -A Win32 .. || goto :error
 
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
