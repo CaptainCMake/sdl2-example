960a10484f3d57caae9101e9d507d4ecebe4c2cd
Steve Robinson <ssrobins@gmail.com>
2019-06-13 20:32:17 -0700

    Change build directory all the way through the script

--- build_windows.bat
+++ build_windows.bat
@@ -2,18 +2,19 @@
 setlocal
 
 set build_dir=build_windows
+set build_path=C:\%build_dir%
 
-cd /d C:\
+cd /d %build_path%
 if not exist %build_dir% mkdir %build_dir%
 cd %build_dir%
 
 dir
 
-conan install .. -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
+conan install %build_path% -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
 
 systeminfo
 
-cmake -G "Visual Studio 16 2019" -A Win32 .. || goto :error
+cmake -G "Visual Studio 16 2019" -A Win32 %build_path% || goto :error
 
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
 
