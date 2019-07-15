f52269d09d5601f18086e9c17ec1d213b2dffdb4
Steve Robinson <ssrobins@gmail.com>
2019-07-14 21:46:39 -0700

    Use 64-bit toolchain

--- build_windows.bat
+++ build_windows.bat
@@ -9,7 +9,7 @@ cd %build_dir%
 
 conan install .. -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
 
-cmake -G "Visual Studio 16 2019" -A Win32 .. || goto :error
+cmake -G "Visual Studio 16 2019" -A Win32 -T host=x64 .. || goto :error
 
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
 
