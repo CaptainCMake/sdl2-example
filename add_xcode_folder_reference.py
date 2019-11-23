c29bb775035621b83f28ed1d0f16c3acea0e2818
Steve Robinson <ssrobins@gmail.com>
2019-11-23 11:20:27 -0800

    Generalize folder reference script so it'll work with xcassets and regular folders

--- add_xcode_folder_reference.py
+++ add_xcode_folder_reference.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import uuid
 
 
 def main():
@@ -15,27 +16,42 @@ def main():
     folder_path = command_args.folderPath
     folder = os.path.basename(folder_path)
     
-    id1 = "101"
-    id2 = "201"
+    id1 = str(uuid.uuid4().hex)
+    id2 = str(uuid.uuid4().hex)
     
     pbxbuildfile_header = '/* Begin PBXBuildFile section */'
     project_data = project_data.replace(
         pbxbuildfile_header,
         '{0}\n\t\t{1} /* {2} in Resources */ = {{isa = PBXBuildFile; fileRef = {3} /* {2} */; }};'.format(pbxbuildfile_header, id1, folder, id2),
     )
+
+    if folder.endswith(".xcassets"):
+        last_known_file_type = "folder.assetcatalog"
+    else:
+        last_known_file_type = "folder"
     
     pbxfilereference_header = '/* Begin PBXFileReference section */'
     project_data = project_data.replace(
         pbxfilereference_header,
-        '{0}\n\t\t{1} /* {2} */ = {{isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; name = {2}; path = {3}; sourceTree = "<group>"; }};'.format(pbxfilereference_header, id2, folder, folder_path)
+        '{0}\n\t\t{1} /* {2} */ = {{isa = PBXFileReference; lastKnownFileType = {3}; name = {2}; path = {4}; sourceTree = "<group>"; }};'.format(pbxfilereference_header, id2, folder, last_known_file_type, folder_path)
     )
     
-    pbxgroup_section = '/* Resources */ = {\n\t\t\tisa = PBXGroup;\n\t\t\tchildren = ('
+    # Get <RESOURCES_PBXGROUP_ID> for 'Resources' in <TARGET>:
+    # <PBXGROUP_ID> /* <TARGET> */ = {
+    #        isa = PBXGroup;
+    #        children = (
+    #            <RESOURCES_PBXGROUP_ID> /* Resources */,
+    index_pbx_group = project_data.find('/* {0} */ = {{\n\t\t\tisa = PBXGroup;'.format(command_args.target))
+    index_resources_child = project_data.find('/* Resources */', index_pbx_group)
+    index_start_resources_pbxgroup = project_data.rfind('\t', 0, index_resources_child)
+    resources_pbxgroup_id = project_data[index_start_resources_pbxgroup:index_resources_child].strip()
+    
+    pbxgroup_section = '{0} /* Resources */ = {{\n\t\t\tisa = PBXGroup;\n\t\t\tchildren = ('.format(resources_pbxgroup_id)
     project_data = project_data.replace(
         pbxgroup_section,
         '{0}\n\t\t\t\t{1} /* {2} */,'.format(pbxgroup_section, id2, folder)
     )
-    
+
     pbxbuildphase_section = 'isa = PBXResourcesBuildPhase;\n\t\t\tbuildActionMask = 2147483647;\n\t\t\tfiles = ('
     project_data = project_data.replace(
         pbxbuildphase_section,
